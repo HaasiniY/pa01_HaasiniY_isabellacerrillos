e342b5ac089ee1095ca0b8e5ad37cd840f652f71
HaasiniY <143018016+HaasiniY@users.noreply.github.com>
2026-02-13 15:16:18 -0800

    added getters

--- card.cpp
+++ card.cpp
@@ -1,4 +1,30 @@
 // card.cpp
 // Authors: Isabella Cerrrilos, Haasini Yakkala 
 // Implementation of the classes defined in card.h
+#include "card.h"
+#include <iostream>
+using namespace std;
 
+Card::Card(char s, char val) : suit(s), value(val){}
+
+char Card::getSuit() const{
+    return suit;
+}
+
+char Card::getValue() const{
+    return value;
+}
+
+bool operator==(const Card& lhs, const Card& rhs){
+
+}
+bool operator<(const Card& lhs, const Card& rhs){
+
+}
+bool operator>(const Card& lhs, const Card& rhs){
+
+}
+ostream& operator<<(ostream& out, const Card& c){
+
+    return out;
+}
