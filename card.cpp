2c81a3a54adc73a55c0d45da55b01000f9c15c83
HaasiniY <143018016+HaasiniY@users.noreply.github.com>
2026-02-13 18:20:26 -0800

    defined operators and getter methods

--- card.cpp
+++ card.cpp
@@ -15,16 +15,74 @@ char Card::getValue() const{
     return value;
 }
 
-bool operator==(const Card& lhs, const Card& rhs){
+int Card::getSuitNum(char c) const{
+    if(c == 'c'){
+        return 0;
+    }
+    else if(c == 's'){
+        return 1;
+    }
+    else if(c == 's'){
+        return 2;
+    }
+    else if(c == 'h'){
+        return 3;
+    }
+    else{
+        return -1;
+    }
+}
 
+int Card::getLetterNum(char val) const{
+    if(val >= 2 && val <=9){
+        return val - '0';
+    }
+    else if(val == 'a'){
+        return 1;
+    }
+    else if(val == 'j'){
+        return 10;
+    }
+    else if(val == 'q'){
+        return 11;
+    }
+    else if(val == 'k'){
+        return 12;
+    }
+    return -1;
+}
+
+
+bool operator==(const Card& lhs, const Card& rhs){
+    if((lhs.getSuitNum(lhs.getSuit()) != rhs.getSuitNum(rhs.getSuit())) 
+    || (lhs.getValue() != rhs.getValue())){
+        return false;
+    }
+    return true;
 }
 bool operator<(const Card& lhs, const Card& rhs){
-
+    if(lhs.getSuitNum(lhs.getSuit()) < rhs.getSuitNum(rhs.getSuit())) {
+        return true;
+    }
+    if(lhs.getSuitNum(lhs.getSuit()) == rhs.getSuitNum(rhs.getSuit())) {
+        if(lhs.getLetterNum() < rhs.getLetterNum()){
+            return true;
+        }
+    }
+    return false;
 }
 bool operator>(const Card& lhs, const Card& rhs){
-
+    if(lhs.getSuitNum(lhs.getSuit()) > rhs.getSuitNum(rhs.getSuit())) {
+        return true;
+    }
+    if(lhs.getSuitNum(lhs.getSuit()) == rhs.getSuitNum(rhs.getSuit())) {
+        if(lhs.getLetterNum() > rhs.getLetterNum()){
+            return true;
+        }
+    }
+    return false;
 }
 ostream& operator<<(ostream& out, const Card& c){
-
+    out<< c.getSuit() << " "<< c.getValue() << endl;
     return out;
 }
